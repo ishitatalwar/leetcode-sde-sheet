348faf2e2b28df2ad0dff7749bdc2a84dcb8d3cc
Ishita Talwar <103492839+ishitatalwar@users.noreply.github.com>
2026-02-13 13:32:38 +0530

    Time: 8 ms (6.38%), Space: 97.4 MB (60.07%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -6,14 +6,20 @@ public:
 
         int minPrice = INT_MAX;
         int maxProfit = 0;
-        for(int i = 0; i<n; i++){
-            if(prices[i] < minPrice){
-                minPrice = prices[i];
-            }
-            else{
-                maxProfit = max(maxProfit, prices[i] - minPrice);
-            }
+        for(int i = 0; i <n ; i++){
+            minPrice = min(minPrice, prices[i]);
+            int profit = prices[i] - minPrice;
+            maxProfit = max(maxProfit, profit);
         }
-        return maxProfit;       
+        // for(int i = 0; i<n; i++){
+        //     if(prices[i] < minPrice){
+        //         minPrice = prices[i];
+        //     }
+        //     else{
+        //         maxProfit = max(maxProfit, prices[i] - minPrice);
+        //     }
+        // }
+        return maxProfit;
+        // return maxProfit;       
     }
 };
