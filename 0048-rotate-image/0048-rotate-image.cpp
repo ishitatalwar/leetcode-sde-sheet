2a811ef3f70c5aa08ab185c69f4686f79e0f591a
Ishita Talwar <103492839+ishitatalwar@users.noreply.github.com>
2026-02-19 14:22:20 +0530

    Time: 0 ms (100%), Space: 10.3 MB (36.1%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        // brute force approach 
+        // optimal approach  
         int n = matrix.size();
-        vector<vector<int>> rotated(n, vector<int>(n));
 
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n ; j++){
-                rotated[j][n-i-1] = matrix[i][j];
+        // transpose the matrix 
+        for(int i = 0; i<n ; i++){
+            for(int j = i+1; j<n ; j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
-        }   
-        matrix = rotated;    
+        }
+
+        // revrse each row
+        for(int i = 0; i<n ; i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
     }
 };
