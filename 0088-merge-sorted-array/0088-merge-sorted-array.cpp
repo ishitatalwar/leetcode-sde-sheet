800fda3a39bc24ee4b8b3864f147c3cd8183620a
Ishita Talwar <103492839+ishitatalwar@users.noreply.github.com>
2026-02-20 12:35:17 +0530

    Time: 0 ms (100%), Space: 12.3 MB (40.84%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,47 +1,21 @@
 class Solution {
 public:
-    void swapIfGreater(vector<int>& nums1, vector<int>& nums2, int ind1, int ind2) {
-        if (nums1[ind1] > nums2[ind2]) {
-            swap(nums1[ind1], nums2[ind2]);
-        }
-    }
-
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        // Length of the combined array
-        int len = m + n;
-
-        // Initial gap
-        int gap = (len / 2) + (len % 2);
-
-        while (gap > 0) {
-            // Place two pointers
-            int left = 0;
-            int right = left + gap;
-            while (right < len) {
-                // Case 1: left in nums1[] and right in nums2[]
-                if (left < m && right >= m) {
-                    swapIfGreater(nums1, nums2, left, right - m);
-                }
-                // Case 2: both pointers in nums2[]
-                else if (left >= m) {
-                    swapIfGreater(nums2, nums2, left - m, right - m);
-                }
-                // Case 3: both pointers in nums1[]
-                else {
-                    swapIfGreater(nums1, nums1, left, right);
-                }
-                left++, right++;
+        int left = m - 1;
+        int right = 0;
+        while(left >= 0 && right < n){
+            if(nums1[left] > nums2[right]){
+                swap(nums1[left], nums2[right]);
+                left--, right++;
             }
-
-            // Break if iteration with gap = 1 is completed
-            if (gap == 1) break;
-
-            // Otherwise, calculate new gap
-            gap = (gap / 2) + (gap % 2);
+            else{
+                break;
+            }
         }
-
-        // Finally, copy nums2 into the remaining part of nums1
-        for (int i = 0; i < n; i++) {
+        sort(nums1.begin(), nums1.begin() + m);
+        sort(nums2.begin(), nums2.begin() + n);
+        // Step 3: Copy nums2 into remaining space of nums1
+        for(int i = 0; i < n; i++){
             nums1[m + i] = nums2[i];
         }
     }
