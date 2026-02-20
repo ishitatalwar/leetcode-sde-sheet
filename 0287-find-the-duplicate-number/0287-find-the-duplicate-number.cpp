6848333948cdc2a12e4aef195ff6e5cb057506d7
Ishita Talwar <103492839+ishitatalwar@users.noreply.github.com>
2026-02-20 16:24:08 +0530

    Time: 0 ms (100%), Space: 65 MB (68.98%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> freq(n+1, 0);
-        for(int i = 0; i<n; i++){
-            if(freq[nums[i]] == 0){
-                freq[nums[i]] += 1;
-            }
-            else{
-                return nums[i];
-            }
+        // optimal approach -> linked cycle detection
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+
+        fast = nums[0];
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+        return fast;
         
     }
 };
